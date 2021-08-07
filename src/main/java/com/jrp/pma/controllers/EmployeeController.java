c6be9540c712c69e809e1910a84f312a40bb37b6
Will Broach <william.broach@gmail.com>
2021-08-07 13:13:28 -0400

    adding new templates

--- src/main/java/com/jrp/pma/controllers/EmployeeController.java
+++ src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.jrp.pma.dao.ProjectRepository;
-import com.jrp.pma.entities.Project;
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
 
 @Controller
-@RequestMapping("/projects")
-public class ProjectController {
+@RequestMapping("/employees")
+public class EmployeeController {
 
 	@Autowired
-	ProjectRepository proRepo;
+	EmployeeRepository empRepo;
 	
 	@GetMapping("/new")
-	public String displayProjectForm(Model model) {
-		model.addAttribute("project", new Project());
-		return "new-project";
+	public String displayEmployeeForm(Model model) {
+		model.addAttribute("employee", new Employee());
+		return "new-employee";
 	}
 	
 	@PostMapping("/save")
-	public String createProject(Project project, Model model) {
-		proRepo.save(project);
-		return "redirect:/projects/new"; 	
+	public String createEmployee(Employee employee, Model model) {
+		empRepo.save(employee);
+		return "redirect:/employees/new";
 	}
 	
 }
